cf0db378bed2acb164df11966d3f3a318cd03b96
William Santos Bitencourt de Sousa <165169533+WilliamCutelo@users.noreply.github.com>
2026-05-15 19:41:46 -0300

    Update Matricula.java

--- src/main/java/com/example/sistemamatricula/model/Matricula.java
+++ src/main/java/com/example/sistemamatricula/model/Matricula.java
@@ -1,4 +1,27 @@
-package com.example.sistemamatricula.model;
+package com.example.sistemamatricula.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "matricula")
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 
 public class Matricula {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_aluno")
+    private Aluno aluno;
+
+    @ManyToOne
+    @JoinColumn(name = "id_turma")
+    private Turma turma;
 }
