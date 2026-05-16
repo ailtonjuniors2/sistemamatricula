a54fe8a3200e05be5af7c7a5ea48b329514cc0c0
William Santos Bitencourt de Sousa <165169533+WilliamCutelo@users.noreply.github.com>
2026-05-16 09:11:50 -0300

    Update Agendamento.java

--- src/main/java/com/example/sistemamatricula/model/Agendamento.java
+++ src/main/java/com/example/sistemamatricula/model/Agendamento.java
@@ -1,4 +1,24 @@
-package com.example.sistemamatricula.model;
+package com.example.sistemamatricula.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "agendamento")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Agendamento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDateTime dataHora;
+
+    @ManyToOne
+    @JoinColumn(name = "id_aluno", nullable = false)
+    private Aluno aluno;
 }
