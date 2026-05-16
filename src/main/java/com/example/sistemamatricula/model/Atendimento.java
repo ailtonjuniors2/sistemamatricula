493f8abdff50fb87a90bf9d487a31ef1cf26fe6c
William Santos Bitencourt de Sousa <165169533+WilliamCutelo@users.noreply.github.com>
2026-05-16 09:28:48 -0300

    Update Atendimento.java

--- src/main/java/com/example/sistemamatricula/model/Atendimento.java
+++ src/main/java/com/example/sistemamatricula/model/Atendimento.java
@@ -1,4 +1,33 @@
-package com.example.sistemamatricula.model;
+package com.example.sistemamatricula.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "atendimento")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Atendimento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDateTime dataHora;
+
+    private String status; // REALIZADO, CANCELADO, FALTOU
+
+    private String descricao;
+
+
+    @OneToOne
+    @JoinColumn(name = "id_agendamento")
+    private Agendamento agendamento;
+
+    @ManyToOne
+    @JoinColumn(name = "id_atendente")
+    private Atendente atendente;
 }
