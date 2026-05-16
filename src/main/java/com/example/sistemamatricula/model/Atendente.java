4429ad04fef02a91be26799c0ec3a269749f0b55
William Santos Bitencourt de Sousa <165169533+WilliamCutelo@users.noreply.github.com>
2026-05-16 09:18:20 -0300

    Update Atendente.java

--- src/main/java/com/example/sistemamatricula/model/Atendente.java
+++ src/main/java/com/example/sistemamatricula/model/Atendente.java
@@ -1,4 +1,31 @@
-package com.example.sistemamatricula.model;
+package com.example.sistemamatricula.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "Atendente")
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 
 public class Atendente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "Nome_Completo", nullable = false)
+    private String nomeCompleto;
+
+    @Column(unique = true, nullable = false)
+    private String cpf;
+
+    @Column(nullable = false)
+    private String email;
+    
+    @Column(nullable = false)
+    private String telefone;
 }
